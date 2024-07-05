bb8878e080be8c6883b21814a517a78d6dc6b3de
dannyzou18 <dzou3814@gmail.com>
2024-07-05 18:08:34 -0400

    set up

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -42,24 +42,7 @@ export default async function Home() {
           </p>
         </div>
 
-        <CrudShowcase />
       </div>
     </main>
-  );
-}
-
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest();
-
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
   );
 }
