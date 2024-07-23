8840b5458f47b741974ccee48575942eca2ed0e1
youssefr26 <ryoussef5087@gmail.com>
2024-07-23 15:00:22 -0400

    started addign querying to users page

--- client/src/app/users/page.tsx
+++ client/src/app/users/page.tsx
@@ -1,5 +1,9 @@
-import { mockUsers } from "./mockUsers";
+"use client";
+
+import { useEffect } from "react";
 import { NextPage } from "next";
+import { useGlobalContext } from "../../../context/GlobalProvider";
+import api from "../../../api/axios";
 
 interface User {
   uid: string;
@@ -8,13 +12,35 @@ interface User {
 }
 
 const UsersPage: NextPage = () => {
-  const users: User[] = mockUsers;
+  const { user, setUser, isLoading, setIsLoading } = useGlobalContext();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setIsLoading(true);
+      try {
+        const response = await api.get("/users", {
+          headers: {
+            Authorization: await user.getIdToken(),
+          },
+        });
+        if (response.status === 200) {
+          setUser(response.data);
+        }
+      } catch (error) {
+        console.error("Failed to fetch users:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, [user, setUser, setIsLoading]);
 
   return (
     <div style={{ padding: "20px" }}>
       <h1 style={{ fontSize: "24px", marginBottom: "20px" }}>Users</h1>
       <ul style={{ listStyleType: "none", padding: 0 }}>
-        {users.map((user) => (
+        {user?.map((user: User) => (
           <li
             key={user.uid}
             style={{ borderBottom: "1px solid #ddd", padding: "10px 0" }}
@@ -28,7 +54,7 @@ const UsersPage: NextPage = () => {
           </li>
         ))}
       </ul>
-    </div>d``
+    </div>
   );
 };
 
