63479badde8a6b82653e692627affcf65cd581a5
youssefr26 <ryoussef5087@gmail.com>
2024-08-02 14:26:40 -0400

    Added prisma schema and updated api

--- client/api/axios.ts
+++ client/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const IP_ADDRESS = process.env.EXPO_PUBLIC_IP_ADDRESS;
 
 const api = axios.create({
-  baseURL: `http://${IP_ADDRESS}:8000`,
+  baseURL: `http://localhost:8000`,
 });
 
 export default api;
