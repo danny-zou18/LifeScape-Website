b92f9d06a473f4720594146140645598790720f3
youssefr26 <ryoussef5087@gmail.com>
2024-08-02 14:30:46 -0400

    updated users page

--- client/src/app/users/page.tsx
+++ client/src/app/users/page.tsx
@@ -6,9 +6,9 @@ import { useGlobalContext } from "../../../context/GlobalProvider";
 import api from "../../../api/axios";
 
 interface User {
-  uid: string;
+  id: string;
   email: string | null;
-  displayName: string | null;
+  username: string | null;
 }
 
 const UsersPage: NextPage = () => {
@@ -18,14 +18,11 @@ const UsersPage: NextPage = () => {
     const fetchUsers = async () => {
       setIsLoading(true);
       try {
-        const response = await api.get("/users", {
-          headers: {
-            Authorization: await user.getIdToken(),
-          },
-        });
+        const response = await api.get("/general/getallusers");
         if (response.status === 200) {
           setUser(response.data);
         }
+        console.log(response.data);
       } catch (error) {
         console.error("Failed to fetch users:", error);
       } finally {
@@ -34,26 +31,34 @@ const UsersPage: NextPage = () => {
     };
 
     fetchUsers();
-  }, [user, setUser, setIsLoading]);
+  }, []);
 
   return (
     <div style={{ padding: "20px" }}>
       <h1 style={{ fontSize: "24px", marginBottom: "20px" }}>Users</h1>
-      <ul style={{ listStyleType: "none", padding: 0 }}>
-        {user?.map((user: User) => (
-          <li
-            key={user.uid}
-            style={{ borderBottom: "1px solid #ddd", padding: "10px 0" }}
-          >
-            <p>
-              <strong>ID:</strong> {user.uid}
-            </p>
-            <p>
-              <strong>Name:</strong> {user.displayName}
-            </p>
-          </li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          {Array.isArray(user) &&
+            user.map((user: User) => (
+              <li
+                key={user.id}
+                style={{ borderBottom: "1px solid #ddd", padding: "10px 0" }}
+              >
+                <p>
+                  <strong>ID:</strong> {user.id}
+                </p>
+                <p>
+                  <strong>Name:</strong> {user.username}
+                </p>
+                <p>
+                  <strong>Email:</strong> {user.email}
+                </p>
+              </li>
+            ))}
+        </ul>
+      )}
     </div>
   );
 };
